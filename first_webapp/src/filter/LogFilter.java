65be85b34d6359fc2147fd507661bf260308a8b5
dir-FY24nikakeirandish <NikaKheirandish(@Dirbato-1361x>
2024-04-19 15:03:26 +0900

    Lesson16 chapter4

---
+++ first_webapp/src/filter/LogFilter.java
@@ -0,0 +1,56 @@
+package filter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.print.attribute.standard.PrinterLocation;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class LogFilter
+ */
+@WebFilter("/*")
+public class LogFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public LogFilter() {
+        super();
+        
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		chain.doFilter(request, response);
+		
+		// アクセスされた場所と日時を記録する
+		System.out.println(((HttpServletRequest) request).getRequestURI() + ":" + LocalDateTime.now());
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
